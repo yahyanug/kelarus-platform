e5e7562f2f2f84b05fdf90263adac2718b71edce
Yahya <yahya.nugroho.dev@gmail.com>
2026-09-23 00:00:22 +0700

    partial update - api-gateway swagger still cannot read account-component

--- component/api-gateway/src/main/java/id/com/flare/kelarus/gateway/component/config/SwaggerConfiguration.java
+++ component/api-gateway/src/main/java/id/com/flare/kelarus/gateway/component/config/SwaggerConfiguration.java
@@ -1,13 +1,13 @@
 package id.com.flare.kelarus.gateway.component.config;
 
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
-import org.springframework.beans.factory.ObjectProvider;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import reactor.core.publisher.Mono;
+import org.springframework.context.annotation.Primary;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import static org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
@@ -17,31 +17,21 @@ import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
 public class SwaggerConfiguration {
 
 	@Bean
+	@Primary
 	@Lazy(false)
-	public Set<SwaggerUrl> apis(
-			ObjectProvider<SwaggerUiConfigParameters> swaggerUiConfigParametersProvider,
-			RouteDefinitionLocator locator) {
+	public SwaggerUiConfigProperties swaggerUiConfig(SwaggerUiConfigProperties config, RouteDefinitionLocator locator) {
 
-		Set<SwaggerUrl> urls = locator.getRouteDefinitions()
-				.flatMap(route -> Mono.justOrEmpty(route.getId()))
-				.filter(routeId -> routeId.endsWith("-component"))
-				.map(serviceName -> new SwaggerUrl(
-						serviceName,
-						"/" + serviceName + DEFAULT_API_DOCS_URL,
-						null))
-				.collectList()
-				.map(Set::copyOf)
-				.blockOptional()
-				.orElseGet(Set::of);
+		Set<SwaggerUrl> urls = locator.getRouteDefinitions().map(route -> {
+			assert route.getId() != null;
+			return route.getId();
+		}).filter(routeId -> routeId.endsWith("-component")).sort()
+				.map(routeId -> new SwaggerUrl(routeId, "/" + routeId + DEFAULT_API_DOCS_URL, null)).collectList()
+				.map(LinkedHashSet::new).blockOptional().orElseGet(LinkedHashSet::new);
 
-		SwaggerUiConfigParameters swaggerUiConfigParameters =
-				swaggerUiConfigParametersProvider.getIfAvailable();
+		config.setUrl(null);
+		config.setUrls(urls);
 
-		if (swaggerUiConfigParameters != null) {
-			swaggerUiConfigParameters.setUrls(urls);
-		}
-
-		return urls;
+		return config;
 	}
 
 }
